f26dbec2b251edf866e16601edada9bfd875d581
ardinbig <ardinbig@gmail.com>
2025-05-16 16:30:01 +0200

    feat(fleet_repository): add initial implementation

---
+++ packages/fleet_repository/lib/fleet_repository.dart
@@ -0,0 +1,4 @@
+/// A Dart package combining remote and local data sources for fleet monitoring.
+library;
+
+export 'src/fleet_repository.dart';
---
+++ packages/fleet_repository/lib/src/fleet_repository.dart
@@ -0,0 +1,7 @@
+/// {@template fleet_repository}
+/// A Dart package combining remote and local data sources for fleet monitoring.
+/// {@endtemplate}
+class FleetRepository {
+  /// {@macro fleet_repository}
+  const FleetRepository();
+}
