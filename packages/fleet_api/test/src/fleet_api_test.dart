5829ab9bac1ff11a6cd81931725c97ef1187c061
ardinbig <ardinbig@gmail.com>
2025-05-21 15:06:23 +0200

    test(fleet_api): add unit tests for `FleetApi` and `Car` models

--- packages/fleet_api/test/src/fleet_api_test.dart
+++ packages/fleet_api/test/src/fleet_api_test.dart
@@ -1,1 +1,19 @@
-// TODO(ardinbig): write the tests for this package
+import 'package:fleet_api/fleet_api.dart';
+import 'package:test/test.dart';
+
+class TestFleetApi extends FleetApi {
+  TestFleetApi() : super();
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return super.noSuchMethod(invocation);
+  }
+}
+
+void main() {
+  group('FleetApi', () {
+    test('can be instantiated', () {
+      expect(TestFleetApi.new, returnsNormally);
+    });
+  });
+}
